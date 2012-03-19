b77e813465b21d5bdeab296017b2e62f0608cc28
kinochen <kino0815@gmail.com>
2012-03-20 03:34:45 +0800

    obj done

--- multitrigger/MessageInterface.ino
+++ multitrigger/MessageInterface.ino
@@ -1,20 +1,30 @@
 
 union message{
-  byte content[2];
-
+ // String read_string; 
+  byte read_byte[3];
+  float  val; 
+  unsigned int valint;
 } msg;
 
 void receiver(){
-  if(Serial.available() > 1 ){ 
-    msg.content[0]=Serial.read();
-    msg.content[1]=Serial.read();
-    
-//    Serial.print(msg.content[0],byte);   
-//    Serial.print(msg.content[1],byte);  
-      checkFirstByte(msg.content[0]);
-      
+  if(Serial.available()>0  ){ // >2
+//    msg.read_byte[0]=Serial.read();
+//    msg.read_byte[1]=Serial.read();
+//    msg.read_byte[2]=Serial.read();
+
+int test = Serial.read();
+
+
+
+     // checkFirstByte(msg.read_byte[0]);
+     
+//        Serial.print(  msg.read_byte[0]);
+//       Serial.print(  msg.read_byte[1]);
+//        Serial.print(  msg.read_byte[2]);
+ Serial.print(test);
+
     }
-  
+
   
 }
 
--- multitrigger/multitrigger.ino
+++ multitrigger/multitrigger.ino
@@ -1,45 +1,31 @@
+
+#include <ICGroup.h>
 // Lab12 使用兩顆 74HC595 和三支腳位控制 16 顆 LED
 
 // 接 74HC595 的 ST_CP (pin 12,latch pin)
-int latchPin = 8;
-// 接 74HC595 的 SH_CP (pin 11, clock pin)
-int clockPin = 12;
-// 接 74HC595 的 DS (pin 14)
-int dataPin = 11;
+//int latchPin = 8;
+//// 接 74HC595 的 SH_CP (pin 11, clock pin)
+//int clockPin = 12;
+//// 接 74HC595 的 DS (pin 14)
+//int dataPin = 11;
 //-------------
 int command;
 
-
+ICGroup g1(2,3,4);
 
 
 
 //--------------
 void setup() {
-  // 將 latchPin, clockPin, dataPin 設置為輸出
-  pinMode(latchPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
+
+  g1.setup();
+  Serial.begin(9600);
 }
 
 void loop() {      
-  for (int led = 0; led < 16; led++) {
-    int numberToDisplay  = 1 << led;
-    byte high_Byte = highByte(numberToDisplay);
-    byte low_Byte = lowByte(numberToDisplay);
-    
-    // 送資料前要先把 latchPin 拉成低電位
-    digitalWrite(latchPin, LOW);
-    
-    // 先送高位元組 (Hight Byte), 給離 Arduino 較遠的那顆 74HC595
-    shiftOut(dataPin, clockPin, MSBFIRST, high_Byte);  
-    // 再送低位元組 (Low Byte), 給離 Arduino 較近的那顆 74HC595
-    shiftOut(dataPin, clockPin, MSBFIRST, low_Byte);  
-
-    // 送完資料後要把 latchPin 拉回成高電位
-    digitalWrite(latchPin, HIGH);
-   
-    delay(40);
-  }
+  receiver();  
+  g1.update();
+  
 }
 //再次提醒：在送資料前，記得要先把 latchPin 拉成低電位，
 //緊接著使用 shiftOut() 函式送出資料，送完資料後還要把 latchPin 拉回高電位。
